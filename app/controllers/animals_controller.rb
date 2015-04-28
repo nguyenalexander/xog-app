d4e9bb27b174df9a9aac34309968dc2949a50e93
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-04-27 20:32:25 -0700

    updated code for barebones site

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,44 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.create(animal_params)
+    redirect_to @animal
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to @animal
+    else render 'edit'
+    end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name,:desc,:species)
+  end
+
+end
---
+++ db/migrate/20150428013208_create_animals.rb
@@ -0,0 +1,11 @@
+class CreateAnimals < ActiveRecord::Migration
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.text :desc
+      t.string :species
+
+      t.timestamps null: false
+    end
+  end
+end
